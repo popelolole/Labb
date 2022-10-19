c1975070ecefbc944a5a4ab0a2cea7a0d3c2244d
popelolole <pellebe@kth.se>
2022-10-19 19:22:23 +0200

    Update ProjectsManager.java

--- Labb3B/src/model/ProjectsManager.java
+++ Labb3B/src/model/ProjectsManager.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class ProjectsManager {
     private int nextProjectId;
-    private ArrayList<Project> projects;
+    private final ArrayList<Project> projects;
 
     public ProjectsManager(){
         this.nextProjectId = 1;
